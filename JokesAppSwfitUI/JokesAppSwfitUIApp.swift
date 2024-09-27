401acb128f056333c5dfb64c0816b87c93c8a735
Sabricetin <sabriyazilim@gmail.com>
2024-09-27 08:49:10 +0300

    Initial Commit

---
+++ JokesAppSwfitUI/JokesAppSwfitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwfitUIApp.swift
+//  JokesAppSwfitUI
+//
+//  Created by Sabri Çetin on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwfitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
